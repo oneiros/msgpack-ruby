002385a66699ed7fc175cb534246a068fdf3727b
David Roetzel <david@roetzel.de>
2022-03-10 12:26:20 +0100

    Fix for ruby >= 2.4

--- ext/msgpack/core_ext.c
+++ ext/msgpack/core_ext.c
@@ -64,17 +64,10 @@ static VALUE FalseClass_to_msgpack(int argc, VALUE* argv, VALUE self)
     return packer;
 }
 
-static VALUE Fixnum_to_msgpack(int argc, VALUE* argv, VALUE self)
+static VALUE Integer_to_msgpack(int argc, VALUE* argv, VALUE self)
 {
     ENSURE_PACKER(argc, argv, packer, pk);
     msgpack_packer_write_fixnum_value(pk, self);
-    return packer;
-}
-
-static VALUE Bignum_to_msgpack(int argc, VALUE* argv, VALUE self)
-{
-    ENSURE_PACKER(argc, argv, packer, pk);
-    msgpack_packer_write_bignum_value(pk, self);
     return packer;
 }
 
@@ -118,8 +111,7 @@ void MessagePack_core_ext_module_init()
     rb_define_method(rb_cNilClass,   "to_msgpack", NilClass_to_msgpack, -1);
     rb_define_method(rb_cTrueClass,  "to_msgpack", TrueClass_to_msgpack, -1);
     rb_define_method(rb_cFalseClass, "to_msgpack", FalseClass_to_msgpack, -1);
-    rb_define_method(rb_cFixnum, "to_msgpack", Fixnum_to_msgpack, -1);
-    rb_define_method(rb_cBignum, "to_msgpack", Bignum_to_msgpack, -1);
+    rb_define_method(rb_cInteger, "to_msgpack", Integer_to_msgpack, -1);
     rb_define_method(rb_cFloat,  "to_msgpack", Float_to_msgpack, -1);
     rb_define_method(rb_cString, "to_msgpack", String_to_msgpack, -1);
     rb_define_method(rb_cArray,  "to_msgpack", Array_to_msgpack, -1);
